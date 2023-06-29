e5270a14a7f53e79a2271f7fae81d692d46dfa14
kien242 <kien242tran@gmail.com>
2023-06-29 23:06:49 +0700

    Update package-lock.json, package.json, App.js, and 2 more files

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,32 +1,42 @@
-import AppStyle from './App.module.scss';
-import * as ShowUser from './Component/ShowUser/index.js'
-import * as ShowRepo from './Component/ShowRepo/index.js'
+
+import { Container} from 'react-bootstrap';
+import ResponsiveDrawer from './Component/nav_test/index.js';
+
 
 function App() {
   return (
-    <div className={`container-fluid ${AppStyle.App}`}>
-      <div className='row'>
-        <div className='col-3'>
-          <ShowUser.showUser
-            UserAvatar="https://avatars.githubusercontent.com/u/44479300?v=4"
-            UserName="zmkfirmware"
-            followers={"14"}
-            following={"15"}
-          />
+    <Container>
+      <ResponsiveDrawer/>
 
-          <ShowUser.showUser
-            UserAvatar="https://avatars.githubusercontent.com/u/44479300?v=4"
-            UserName="zmkfirmware"
-            followers={"14"}
-            following={"15"}
-          />
+      {/* <div className='row'>
+        <div className='col-3'>
+          <div>
+            <ListGroup>
+              <ListGroupItem>
+                <ShowUser.showUser
+                  UserAvatar="https://avatars.githubusercontent.com/u/44479300?v=4"
+                  UserName="zmkfirmware"
+                  followers={"14"}
+                  following={"15"}
+                />
+              </ListGroupItem>
+              <ListGroupItem>
+                <ShowUser.showUser
+                  UserAvatar="https://avatars.githubusercontent.com/u/44479300?v=4"
+                  UserName="zmkfirmware"
+                  followers={"14"}
+                  following={"15"}
+                />
+              </ListGroupItem>
+            </ListGroup>
+          </div>
         </div>
         <div className='col-9'>
           <ShowRepo.showRepo />
           <ShowRepo.showRepo />
         </div>
-      </div>
-    </div>
+      </div> */}
+    </Container>
   );
 }
 
