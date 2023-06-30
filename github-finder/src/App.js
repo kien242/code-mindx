f057605038059950c0d1aca85bdaf51caf39d1a0
Tran Kien <kien242@kien242-Laptop>
2023-06-30 18:48:52 +0700

    Merge branch 'main' of git@github.com:kien242/code-mindx.git
    Conflicts:
    	github-finder/src/App.js

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,9 +1,11 @@
-import AppStyle from './App.module.scss';
-import * as ShowUser from './Component/ShowUser/index.js'
-import * as ShowRepo from './Component/ShowRepo/index.js'
+
+import { Container} from 'react-bootstrap';
+import ResponsiveDrawer from './Component/nav_test/index.js';
+
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className={`container-fluid ${AppStyle.App}`}>
       <div className='row'>
         <div className='col-4'>
@@ -13,20 +15,40 @@ function App() {
             followers={"14"}
             following={"15"}
           />
+=======
+    <Container>
+      <ResponsiveDrawer/>
+>>>>>>> main
 
-          <ShowUser.showUser
-            UserAvatar="https://avatars.githubusercontent.com/u/44479300?v=4"
-            UserName="zmkfirmware"
-            followers={"14"}
-            following={"15"}
-          />
+      {/* <div className='row'>
+        <div className='col-3'>
+          <div>
+            <ListGroup>
+              <ListGroupItem>
+                <ShowUser.showUser
+                  UserAvatar="https://avatars.githubusercontent.com/u/44479300?v=4"
+                  UserName="zmkfirmware"
+                  followers={"14"}
+                  following={"15"}
+                />
+              </ListGroupItem>
+              <ListGroupItem>
+                <ShowUser.showUser
+                  UserAvatar="https://avatars.githubusercontent.com/u/44479300?v=4"
+                  UserName="zmkfirmware"
+                  followers={"14"}
+                  following={"15"}
+                />
+              </ListGroupItem>
+            </ListGroup>
+          </div>
         </div>
         <div className='col-8'>
           <ShowRepo.showRepo />
           <ShowRepo.showRepo />
         </div>
-      </div>
-    </div>
+      </div> */}
+    </Container>
   );
 }
 
