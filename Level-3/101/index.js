8913586692be6b6b62222de6a900ee4d0f0a3d14
Tran Kien <kien242@KienTran-Ubuntu-Desktop>
2023-08-28 20:33:04 +0700

    Update .gitignore, index.js, index.ts, and 2 more files

--- Level-3/101/index.js
+++ Level-3/101/index.js
@@ -1,6 +1,14 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const port = 3000;
+
+// Handling '/' Request
+app.get("/:id", (req, res) => {
+  const param = req.params;
+  console.log(param);
+  res.send("TypeScript With Express");
+  console.log(`TypeScript with Express http://localhost:${port}/`);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
