4ad8fa709d8d53a5e1e727196b8aeedc0bf0fce7
Tran Kien <kien242@KienTran-Ubuntu-Desktop>
2023-09-07 21:52:04 +0700

    Update index.js

--- Level-3/101/index.js
+++ Level-3/101/index.js
@@ -1,13 +1,48 @@
 import express from "express";
 const app = express();
-const port = 3000;
+const port = 2402;
 
+app.use(express.json());
+
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Handling '/' Request
-app.get("/:id", (req, res) => {
-  const param = req.params;
-  console.log(param);
-  res.send("TypeScript With Express");
-  console.log(`TypeScript with Express http://localhost:${port}/`);
+
+app.post("/data", (req, res) => {
+  const { reqHeader } = req.headers;
+  const { newValue } = req.body;
+  if (!newValue) {
+    res.send({ message: "Chua cap gia tri" });
+  } else {
+    data.push(newValue);
+    res.send({
+      message: "thanh cong",
+      newValue: newValue,
+    });
+  }
+});
+
+app.get("/data", (req, res) => {
+  const { option } = req.query;
+  switch (option) {
+    case "LT10":
+      res.send({
+        message: "Thành công",
+        data: data.filter((item) => item > 5),
+      });
+      break;
+    case "GT10":
+      res.send({
+        message: "Thành công",
+        data: data.filter((item) => item < 5),
+      });
+      break;
+    default:
+      res.send({
+        message: "Thành công",
+        data: data,
+      });
+      break;
+  }
 });
 
 app.listen(port, () => {
