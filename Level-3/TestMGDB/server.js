47ed1ff22bd3fc006cd7afe619f84ff9b18e7fc3
DESKTOP-OBQDJG4\Tran Kien <kien242tran@gmail.com>
2023-10-02 17:50:52 +0700

    update

--- Level-3/TestMGDB/server.js
+++ Level-3/TestMGDB/server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import dbFunction from "./src/database/index.js";
-import teacherRoute from "./src/routers/teacher-routers.js";
-import studentRoute from "./src/routers/student-routers.js";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
@@ -14,10 +13,11 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use("/teacher", teacherRoute);
-app.use("/student", studentRoute);
-
-app.listen(process.env.PORT || 2402, () => {
-  dbFunction.connectToDb(); // connect to database
+app.listen(process.env.PORT || 2402, async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/quanlynhatro");
+  } catch (error) {
+    console.log(error);
+  }
   console.log("App listen on PORT", process.env.PORT);
 });
